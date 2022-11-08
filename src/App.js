90a958e1bd83da49d236667a68c233b570cb3fd6
Mina Eskandar <mina.aiad@ejust.edu.eg>
2022-11-08 16:44:57 -0500

    created UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ReactDOM } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Sudoku extends React.Component{
+  render(){
+    return (
+    <div className='center'>
+        <div className='board'>
+        <Board />
+        </div>
+        <div>
+          <button> Solve </button>
+        </div>
+
     </div>
   );
+  }
 }
 
-export default App;
+class Board extends React.Component{
+  // it will have 9 3x3 section
+  render(){
+    //ReactDOM.render('div',)
+    return (<div>
+        <div className="section-row">
+          <Section value="1" />
+          <Section value="2" />
+          <Section value="3" />
+        </div>
+        <div className="section-row">
+          <Section value="4" />
+          <Section value="5" />
+          <Section value="6" />
+        </div>
+        <div className="section-row">
+          <Section value="7" />
+          <Section value="8" />
+          <Section value="9"/>
+        </div>
+    </div>);
+  }
+}
+
+class Section extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      values:Array(9).fill('')
+    }
+  }
+
+  editBox(i,value){
+      let values = this.state.values.slice()
+      values[i] = value
+      this.setState({values:values})
+  }
+  renderBox(i){
+    
+    return (<input type="text" className='box' maxLength="1" value={this.state.values[i]}
+              onBeforeInput={()=>{this.editBox(i,'')}} onInput={(e)=>{this.editBox(i,e.target.value)}}></input>)
+  }
+  render() {
+    
+    return (
+      <div className='section'>
+        <div className="box-row">
+          {this.renderBox(1)}
+          {this.renderBox(2)}
+          {this.renderBox(3)}
+        </div>
+        <div className="box-row">
+          {this.renderBox(4)}
+          {this.renderBox(5)}
+          {this.renderBox(6)}
+        </div>
+        <div className="box-row">
+          {this.renderBox(7)}
+          {this.renderBox(8)}
+          {this.renderBox(9)}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default Sudoku;
