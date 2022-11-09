3c0301cc3fb7b5c681ebebeaf2a58925a02f6954
Mina Eskandar <mina.aiad@ejust.edu.eg>
2022-11-08 19:02:09 -0500

    Duplicate detection in rows and sections

--- src/App.js
+++ src/App.js
@@ -19,24 +19,49 @@ class Sudoku extends React.Component{
 }
 
 class Board extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      // 9x9 grid
+      sections: Array(9).fill(0).map(()=> (Array(9).fill('')))
+    }
+  }
+  editBox = (Sectionidx,Boxidx,value)=>{
+    let sections = this.state.sections.slice()
+    sections[Sectionidx][Boxidx] = value
+    this.setState({sections:sections})
+}
+  
+  renderSection(i){
+    // finding duplicates within section
+    let duplicates = [...duplicates_in(this.state.sections[i-1]),
+                      ...duplicates_in_row(i,this.state.sections)]
+
+    // finding in rows
+
+
+    console.log(`Duplicates in section ${i}: ${duplicates}`)
+
+    return <Section duplicates={duplicates} sectionNum={i} values={this.state.sections[i-1]} editBox={this.editBox}/>
+  }
   // it will have 9 3x3 section
   render(){
     //ReactDOM.render('div',)
     return (<div>
         <div className="section-row">
-          <Section value="1" />
-          <Section value="2" />
-          <Section value="3" />
+          {this.renderSection(1)}
+          {this.renderSection(2)}
+          {this.renderSection(3)}
         </div>
         <div className="section-row">
-          <Section value="4" />
-          <Section value="5" />
-          <Section value="6" />
+          {this.renderSection(4)}
+          {this.renderSection(5)}
+          {this.renderSection(6)}
         </div>
         <div className="section-row">
-          <Section value="7" />
-          <Section value="8" />
-          <Section value="9"/>
+          {this.renderSection(7)}
+          {this.renderSection(8)}
+          {this.renderSection(9)}
         </div>
     </div>);
   }
@@ -44,22 +69,19 @@ class Board extends React.Component{
 
 class Section extends React.Component{
 
-  constructor(props){
-    super(props)
-    this.state = {
-      values:Array(9).fill('')
-    }
-  }
-
-  editBox(i,value){
-      let values = this.state.values.slice()
-      values[i] = value
-      this.setState({values:values})
-  }
-  renderBox(i){
+  renderBox(BoxNum){
+    let sec_idx = this.props.sectionNum -1
     
-    return (<input type="text" className='box' maxLength="1" value={this.state.values[i]}
-              onBeforeInput={()=>{this.editBox(i,'')}} onInput={(e)=>{this.editBox(i,e.target.value)}}></input>)
+    let box_idx = BoxNum-1
+
+    let classes = ""
+    if(this.props.duplicates.indexOf(box_idx) !== -1) {classes += "incorrect "}
+    
+
+    return (<input type="text" className={'box ' + classes} maxLength="1" 
+              value={this.props.values[box_idx]}
+              onBeforeInput={()=>{this.props.editBox(sec_idx,box_idx,'')}}
+              onInput={(e)=>{this.props.editBox(sec_idx,box_idx,e.target.value)}}></input>)
   }
   render() {
     
@@ -85,7 +107,51 @@ class Section extends React.Component{
   }
 }
 
+/**
+ * @param {int[]} section - Array of numbers of length 9
+ * 
+ * @return {int[]} duplicates - array of duplicate indexes
+*/
 
+function duplicates_in(section){
+  let tracker = {}
+  let duplicateIndex = []
+  // debugger  
+  for(let i=0; i<9 ; i++){
+    if(section[i] !== ''){
+      tracker[section[i]] = tracker[section[i]]? tracker[section[i]]+1 : 1
 
+      if(tracker[section[i]] >= 2){
+        duplicateIndex.push(i)
+        duplicateIndex.push(section.indexOf(section[i]))
+      }
+      
+    }
+  }
+  return duplicateIndex 
+}
+
+function duplicates_in_row(sectionidx, sections){
+
+  let duplicates = []
+  if(sectionidx===2)debugger
+  for(let j=0;j<3;j++){
+    // We need to have a function that f(x)= 0 for x = 1,2,3, =3 for 4,5,6 and =6 for 7,8,9
+    let row_idx = sectionidx<4?0:(sectionidx<7?3:6)
+    let row = [...sections[row_idx].slice(j*3, j*3 + 3),
+      ...sections[row_idx+1].slice(j*3, j*3 + 3),
+      ...sections[row_idx+2].slice(j*3, j*3 + 3)]
+    
+    let duplicates_in_row = duplicates_in(row);
+    // now we have duplicates in row
+    let sect_row = sections[sectionidx-1].slice(j*3, j*3 + 3)
+    for(let i=0; i<duplicates_in_row.length;i++){
+      let idx = sect_row.indexOf(row[duplicates_in_row[i]])
+      if(idx !== -1)duplicates.push(idx + j*3)
+    }
+  }
+
+  return duplicates
+}
 
 export default Sudoku;
