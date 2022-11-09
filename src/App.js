656d5423fbf69f9a386a0a7ac2d9e9d9a31c6cff
Mina Eskandar <mina.aiad@ejust.edu.eg>
2022-11-08 22:54:50 -0500

    Vertical duplicate detection

--- src/App.js
+++ src/App.js
@@ -3,22 +3,6 @@ import { ReactDOM } from 'react';
 import './App.css';
 
 class Sudoku extends React.Component{
-  render(){
-    return (
-    <div className='center'>
-        <div className='board'>
-        <Board />
-        </div>
-        <div>
-          <button> Solve </button>
-        </div>
-
-    </div>
-  );
-  }
-}
-
-class Board extends React.Component{
   constructor(props){
     super(props)
     this.state={
@@ -31,18 +15,36 @@ class Board extends React.Component{
     sections[Sectionidx][Boxidx] = value
     this.setState({sections:sections})
 }
+  render(){
+    return (
+    <div className='center'>
+        <div className='board'>
+        <Board sections={this.state.sections} editBox={this.editBox}/>
+        </div>
+        <div className='buttons'>
+          <button> Solve </button>
+          <button onClick={()=>{this.setState({ sections: Array(9).fill(0).map(()=> (Array(9).fill('')))
+})}}> Reset </button>
+        </div>
+
+    </div>
+  );
+  }
+}
+
+class Board extends React.Component{
   
   renderSection(i){
     // finding duplicates within section
-    let duplicates = [...duplicates_in(this.state.sections[i-1]),
-                      ...duplicates_in_row(i,this.state.sections)]
+    let duplicates = [...duplicates_in(this.props.sections[i-1]),
+                      ...duplicates_in_row(i,this.props.sections),
+                      ...duplicates_in_col(i,this.props.sections)]
 
     // finding in rows
 
 
-    console.log(`Duplicates in section ${i}: ${duplicates}`)
 
-    return <Section duplicates={duplicates} sectionNum={i} values={this.state.sections[i-1]} editBox={this.editBox}/>
+    return <Section duplicates={duplicates} sectionNum={i} values={this.props.sections[i-1]} editBox={this.props.editBox}/>
   }
   // it will have 9 3x3 section
   render(){
@@ -134,8 +136,8 @@ function duplicates_in(section){
 function duplicates_in_row(sectionidx, sections){
 
   let duplicates = []
-  if(sectionidx===2)debugger
   for(let j=0;j<3;j++){
+    // j is row
     // We need to have a function that f(x)= 0 for x = 1,2,3, =3 for 4,5,6 and =6 for 7,8,9
     let row_idx = sectionidx<4?0:(sectionidx<7?3:6)
     let row = [...sections[row_idx].slice(j*3, j*3 + 3),
@@ -146,8 +148,33 @@ function duplicates_in_row(sectionidx, sections){
     // now we have duplicates in row
     let sect_row = sections[sectionidx-1].slice(j*3, j*3 + 3)
     for(let i=0; i<duplicates_in_row.length;i++){
+      // j=1, idx = 1
+
       let idx = sect_row.indexOf(row[duplicates_in_row[i]])
       if(idx !== -1)duplicates.push(idx + j*3)
+    }
+  }
+
+  return duplicates
+}
+function duplicates_in_col(sectionidx, sections){
+
+  let duplicates = []
+  if(sectionidx===5)debugger
+  for(let j=0;j<3;j++){
+    // j is column number
+    // We need to have a function that f(x)= 0 for x = 1,4,7; =1 for 2,5,8 and =2 for 3,6,9
+    let col_idx = (sectionidx-1)%3
+    let col = [...sections[col_idx].reduce((a,b,i)=> {if(i%3 === j){a.push(b)} return a},[]),
+      ...sections[col_idx+3].reduce((a,b,i)=> {if(i%3 === j){a.push(b)} return a},[]),
+      ...sections[col_idx+6].reduce((a,b,i)=> {if(i%3 === j){a.push(b)} return a},[])]
+    
+    let duplicates_in_col = duplicates_in(col);
+    // now we have duplicates in row
+    let sect_col = sections[sectionidx-1].reduce((a,b,i)=> {if(i%3 === j){a.push(b)} return a},[])
+    for(let i=0; i<duplicates_in_col.length;i++){
+      let idx = sect_col.indexOf(col[duplicates_in_col[i]])
+      if(idx !== -1)duplicates.push(idx*3 + j)
     }
   }
 
